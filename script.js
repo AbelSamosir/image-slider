f9ce3ab393e3cceac3fbcce9efdbcaa0b1fba1ad
AbelSamosir <abel.samosiraja@gmail.com>
2025-02-08 08:52:41 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let allslides = document.getElementsByClassName("slide");
+let navlinks = document.getElementsByClassName("slider__navlink");
+let currentSlide = 0;
+
 const slides = document.querySelectorAll('.slide')
 
 for (const slide of slides) {
@@ -11,3 +15,31 @@ for (const slide of slides) {
 function clearActiveClasses() {
   slides.forEach(slide => slide.classList.remove('active'))
 }
+
+document.getElementById("nav-button--next").addEventListener("click", () => {
+  changeSlide(currentSlide + 1)
+});
+document.getElementById("nav-button--prev").addEventListener("click", () => {
+  changeSlide(currentSlide - 1)
+});
+
+function changeSlide(moveTo) {
+  if (moveTo >= allslides.length) {moveTo = 0;}
+  if (moveTo < 0) {moveTo = allslides.length - 1;}
+  
+  allslides[currentSlide].classList.toggle("active");
+  navlinks[currentSlide].classList.toggle("active");
+  
+ allslides[moveTo].classList.toggle("active");
+  navlinks[moveTo].classList.toggle("active");
+  
+  currentSlide = moveTo;
+}   
+
+document.querySelectorAll('.slider__navlink').forEach((bullet, bulletIndex) => {
+  bullet.addEventListener('click', () => {
+      if (currentSlide !== bulletIndex) {
+          changeSlide(bulletIndex);
+      }
+  })
+})
